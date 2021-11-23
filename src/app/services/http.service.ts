6aa3daaec2a191999043d9e21dc2274716fe5d60
Ahmed Naeem <ahmedtahami@yahoo.com>
2021-11-23 11:02:45 -0800

    [Added] HttpService, Interfaces

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment as env } from 'src/environments/environment';
+import { APIResponse, Game } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getGameList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
+    let params = new HttpParams().set('ordering', ordering);
+    if (search) {
+      params = new HttpParams().set('ordering', ordering).set('search', search);
+    }
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/ganes`, { params : params });
+  }
+}
