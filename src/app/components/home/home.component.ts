4e52de75f58ec643294d44e6e9dd86a42a3f80d7
Ahmed Naeem <ahmedtahami@yahoo.com>
2021-11-24 05:58:57 -0800

    Refactoring in HomeComponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse, Game } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -8,13 +9,16 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public sort : string;
   public games: Array<Game>;
-  constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute) { }
+  private routeSub: Subscription;
+  private gameSub: Subscription;
+
+  constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params : Params) => {
+    this.routeSub = this.activatedRoute.params.subscribe((params : Params) => {
       if(params['game-search'])
       {
         this.searchGames('metacrit');
@@ -25,13 +29,27 @@ export class HomeComponent implements OnInit {
     });
   }
   searchGames(sort: string, search?: string): void {
-    this.httpService
+    this.gameSub = this.httpService
       .getGameList(sort, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
         console.log(gameList); 
       });
   }
-  
 
+  openGameDetails(id : string): void{
+    this.router.navigate(['/details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if(this.gameSub)
+    {
+      this.gameSub.unsubscribe();
+    }
+
+    if(this.routeSub)
+    {
+      this.routeSub.unsubscribe();
+    }
+  }
 }
